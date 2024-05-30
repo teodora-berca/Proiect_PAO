7008bdc1bd09df852ba97dd7552678cbb69f31d4
teodora-berca <berca.teodora@gmail.com>
2024-05-30 18:20:58 +0300

    baza de date

---
+++ src/jdbc/configuration/DatabaseConfiguration.java
@@ -0,0 +1,37 @@
+package jdbc.configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfiguration {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/bdproiect01";
+    private static final String USER = "root";
+    private static final String PASSWORD = "MySQLPa55";
+
+    private static Connection databaseConnection;
+
+    private DatabaseConfiguration() {
+    }
+
+    public static Connection getDatabaseConnection() {
+        try {
+            if (databaseConnection == null || databaseConnection.isClosed()) {
+                databaseConnection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return databaseConnection;
+    }
+
+    public static void closeDatabaseConnection() {
+        try {
+            if (databaseConnection != null && !databaseConnection.isClosed()) {
+                databaseConnection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
